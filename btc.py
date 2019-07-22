b2d663c64c35ed0974e9650516a2b3e12e52e2b5
hilljtm <47020214+hilljtm@users.noreply.github.com>
2019-07-22 18:32:53 -0400

    first commit

---
+++ btc.py
@@ -0,0 +1,31 @@
+# %%
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.dates as mdates
+plt.style.use('ggplot')
+
+
+# import csv
+df = pd.read_csv('BTCUSD.csv')
+
+# drop columns not needed and store in new DF
+df = df.drop(['Adj_Close'], axis=1)
+df = df.drop(['Volume'], axis=1)
+
+
+# plot a lineplot using our DF
+fig, ax = plt.subplots()
+ax.plot('Date', 'Close', data=df)
+
+
+# x-axis
+
+
+ax.set(xlabel='Date (2019)', ylabel='Price (USD)',
+       title='Bitcoin so far in 2019')
+
+ax.grid(True)
+fig.tight_layout()
+fig.autofmt_xdate()
+plt.show()
